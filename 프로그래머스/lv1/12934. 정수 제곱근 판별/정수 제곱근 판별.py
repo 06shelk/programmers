46868ecfaf77c7f4fe65b1f8e973fb50c9499fa6
06shelk <88700824+06shelk@users.noreply.github.com>
2023-09-24 17:14:29 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -0,0 +1,14 @@
+
+def solution(n):
+    answer = 0
+    
+    num = n**(1/2)
+    
+    
+    if num % 1 == 0:
+        answer = (num+1) ** 2
+    
+    else:
+        answer = -1
+            
+    return answer
