8fba35095a94852b9e2abd1031070ce803603108
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-03 22:51:08 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    var answer = 0;
+
+    var total = 0;
+    
+    for(var i=0; i<numbers.length; i++) {
+        total += numbers[i];
+    }
+    
+    answer = total / numbers.length;
+    
+    return answer;
+}
