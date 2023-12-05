d083def5c70a402e975832bf36085c5d21b26a08
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-05 21:00:11 +0900

    [level unrated] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,7 +1,5 @@
 function solution(array, n) {
-    var answer = 0;
-    
-    var count = 0;
+    var answer, count = 0;
     
     for(var i =0; i < array.length; i++) {
         if(n == array[i]) {
