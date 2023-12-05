f9369c195bfa549fbccbcf9c3907a728078ba42b
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-05 13:40:47 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -0,0 +1,21 @@
+function solution(sides) {
+    var answer = 0;
+    
+//     max = Math.max(...sides);
+    
+//     maxNot = 0;
+    
+//     for(var i=0; i<sides.length; i++) {
+//         if(sides[i] != max) {
+//             maxNot += sides[i];
+//         }
+//     }
+    
+//     answer = maxNot > max ? 1 : 2;
+    
+    sides.sort();
+    
+    answer = sides[2] >= sides[0] +  sides[1] ? 2: 1;
+    
+    return answer;
+}
