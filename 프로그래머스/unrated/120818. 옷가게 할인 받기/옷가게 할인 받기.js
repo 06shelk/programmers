9750fabc9b7c9d475ab086dfd5e29a40224fd8ae
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-06 23:45:09 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -2,14 +2,14 @@ function solution(price) {
     var answer = 0;
     
     if(price >= 500000) {
-        answer =  price - price * 0.2;
+        answer =  parseInt(price - price * 0.2);
     }else if(price >= 300000){
-        answer = price - price * 0.1;
+        answer = parseInt(price - price * 0.1);
     }else if (price >= 100000) {
-        answer = price - price * 0.05;
+        answer = parseInt(price - price * 0.05);
     }else {
         answer = price;
     }
     
-    return parseInt(answer);
+    return answer;
 }
