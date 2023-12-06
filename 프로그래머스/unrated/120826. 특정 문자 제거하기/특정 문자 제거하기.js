694dd7e3ab26f42787cc11163207569e8d24d1ab
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-07 08:59:52 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,11 +1,3 @@
 function solution(my_string, letter) {
-    var answer = '';
-    
-    for(var i =0; i < my_string.length; i++) {
-        if (my_string[i] != letter) {
-            answer += my_string[i];
-        }
-    }
-    
-    return answer;
+	return my_string.replaceAll(letter, "");
 }
