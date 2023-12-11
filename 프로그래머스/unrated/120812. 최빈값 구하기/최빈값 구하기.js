6a334f2da2fc8d1a9c31238f74072cf96425a00c
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-11 11:52:35 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,5 +1,5 @@
 function solution(array) {
-    var temp = new Array(1001).fill(0); // 배열의 원소는 0부터 1000까지이므로 길이를 1001로 설정
+    var temp = new Array(1001).fill(0); 
 
     for (var i = 0; i < array.length; i++) {
         temp[array[i]]++;
@@ -8,8 +8,8 @@ function solution(array) {
     var max = Math.max(...temp);
     
     if (temp.filter(value => value === max).length > 1) {
-        return -1; // 최빈값이 여러 개인 경우 -1을 바로 반환
+        return -1;
     }
     
-    return temp.indexOf(max); // 최빈값의 인덱스 반환
+    return temp.indexOf(max);
 }
