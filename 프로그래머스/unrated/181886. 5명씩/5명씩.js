39034e52b394dc77664f80eb5405425f8bb5bc70
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-12 11:29:03 +0900

    [level unrated] Title: 5명씩, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.js
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.js
@@ -2,7 +2,7 @@ function solution(names) {
     var answer = [];
     
     for(var i =0; i<names.length; i++) {
-        if(i%5==0 || i ==0) {
+        if(i%5==0) {
             answer.push(names[i]);
         }
     }
