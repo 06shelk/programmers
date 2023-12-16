6a8f48728300a59f576c58292fcd884774dc2de6
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-16 10:31:30 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,14 +1,14 @@
 function solution(num_list) {
     var answer = 0;
     
-    var a = 0, b =0; 
+    var even = 0, odd =0; 
     
      for(var i=0; i<num_list.length; i++) {
-         if(i%2==0) a += num_list[i];
-         if(i%2==1) b += num_list[i];
+         if(i%2==0) even += num_list[i];
+         if(i%2==1) odd += num_list[i];
      }
     
-    answer = (a>b) ? a: b;
+    answer = (even>odd) ? even: odd;
     
     return answer;
 }
