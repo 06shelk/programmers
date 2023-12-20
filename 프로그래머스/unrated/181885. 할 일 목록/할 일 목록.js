d0e25ee6e55ffa99a6cac9e3ce59c03cf8eebed0
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-20 18:27:01 +0900

    [level unrated] Title: 할 일 목록, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -1,11 +1,9 @@
 function solution(todo_list, finished) {
     var answer = [];
     
-    var finCheck = [];
-    
     for(var i =0; i<finished.length; i++) {
-        if(finished[i]=== false) finCheck.push(todo_list[i]);
+        if(finished[i]=== false) answer.push(todo_list[i]);
     }
     
-    return finCheck;
+    return answer;
 }
