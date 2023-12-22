31b30f96df90fdd976040a335a3c3b9847815a2d
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-22 19:54:29 +0900

    [level unrated] Title: 커피 심부름, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.js
@@ -0,0 +1,15 @@
+function solution(order) {
+    var answer = 0;
+    
+    for(var i =0; i<order.length; i++) {
+       if(order[i].includes("cafelatte")) {
+           answer += 5000;
+       }else if(order[i].includes("cafelatte")) {
+           answer += 4500;
+       }else {
+           answer += 4500;
+       }
+    }
+    
+    return answer;
+}
