065a6d4e8978faddaeacd63bc41d3fd60dcfe185
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-24 22:52:47 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    var answer = 0;
+    
+    var add = "", even ="";
+    
+    for(var i=0; i<num_list.length; i++) {
+      if(num_list[i]%2==0) {
+          even += num_list[i].toString();
+      }else {
+          add += num_list[i].toString();
+      }
+    }
+    
+    answer = Number(even) + Number(add);
+    
+    return answer;
+}
