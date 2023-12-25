7a306fee3f35eeb3bd5af5084275655df3fa080f
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-25 15:39:03 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,7 +1,7 @@
 function solution(myString, pat) {
     var answer = 0;
     
-    answer = (myString.toLowerCase().includes(pat.toLowerCase()) === true) ? 1 : 0;
+    answer = (myString.toLowerCase().includes(pat.toLowerCase())) ? 1 : 0;
     
     return answer;
 }
