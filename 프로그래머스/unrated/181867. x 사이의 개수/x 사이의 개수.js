85ac01ed2b4894afd5bd6067e2dd2e8a63319d85
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-26 19:20:44 +0900

    [level unrated] Title: x 사이의 개수, Time: 3.12 ms, Memory: 38.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.js
@@ -0,0 +1,12 @@
+function solution(myString) {
+    var answer = [];
+    
+    var words = [];
+    words = myString.split("x");
+    
+    for(var i=0; i< words.length; i++) {
+        answer.push(words[i].length); 
+    }
+    
+    return answer;
+}
