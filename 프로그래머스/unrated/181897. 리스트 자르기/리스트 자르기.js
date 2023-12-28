7fb4685d93b34eaefc777ce027a8c10d48c68a46
06shelk <88700824+06shelk@users.noreply.github.com>
2023-12-28 18:35:50 +0900

    [level unrated] Title: 리스트 자르기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,19 @@
+function solution(n, slicer, num_list) {
+    var answer = [];
+    
+    if(n==1) {
+        answer = num_list.slice(0, slicer[1]+1);
+    }else if(n == 2){
+        answer = num_list.slice(slicer[0], num_list.length+1);
+    }else if(n == 3){
+        answer = num_list.slice(slicer[0], slicer[1]+1);
+    }else {
+        
+        num_list = num_list.slice(slicer[0], slicer[1]+1);
+        for(var i=0; i<num_list.length; i+=2) {
+            answer.push(num_list[i]);
+        }
+    }
+    
+    return answer;
+}
