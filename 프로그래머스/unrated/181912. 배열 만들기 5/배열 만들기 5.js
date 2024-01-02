9482fa78a4032aab394e719e6d4077a7fd414732
06shelk <88700824+06shelk@users.noreply.github.com>
2024-01-02 19:11:38 +0900

    [level unrated] Title: 배열 만들기 5, Time: 0.93 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.js
@@ -0,0 +1,9 @@
+function solution(intStrs, k, s, l) {
+    var answer = [];
+    
+    for(var i=0; i<intStrs.length; i++) {     
+        if(k < Number(intStrs[i].slice(s,s+l))) answer.push(Number(intStrs[i].slice(s,s+l)));
+    }
+    
+    return answer;
+}
