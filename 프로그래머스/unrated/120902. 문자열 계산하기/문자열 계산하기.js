d74f8d1e8ad404c42d9f7356df3f1cf8ae5e5dea
06shelk <88700824+06shelk@users.noreply.github.com>
2024-01-04 17:40:28 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,25 @@
+function solution(my_string) {
+    var answer = 0;
+    // 입력값 〉  = "3 + 4 - 2";
+    // 기댓값 〉 5
+
+    my_string = my_string.split(" ");
+    
+    for(var i=0; i<my_string.length; i++) {
+        switch(my_string[i]) {
+            case "+": 
+                answer += Number(my_string[i+1]); 
+                break;
+            case "-": 
+                answer -= Number(my_string[i+1]); 
+                break;
+            default:  
+                break;
+        }
+    }
+    
+    // 첫 번째 숫자 추가
+    answer += Number(my_string[0]);
+
+    return answer;
+}
