b1d8a805902e53806f1d533d81876472ac584b49
06shelk <88700824+06shelk@users.noreply.github.com>
2024-01-06 23:49:07 +0900

    [level unrated] Title: 배열 만들기 6, Time: 3.49 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,20 @@
+function solution(arr) {
+    var answer = [];
+    
+    for(var i =0; i<arr.length; i++) {
+        if(answer.length == 0) {
+            answer.push(arr[i]);
+        } else {
+            if(answer[answer.length-1] == arr[i]) {
+                answer.pop();
+            }else {
+                answer.push(arr[i]);
+            }
+        }
+    }
+    
+    if(answer.length == 0) answer.push(-1);
+    
+    
+    return answer;
+}
