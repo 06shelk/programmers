83e5baa672d524db367d793aab900d38e27bf5d8
06shelk <88700824+06shelk@users.noreply.github.com>
2024-01-18 12:39:33 +0900

    [level Lv.1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/Lv.1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/Lv.1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,10 +1,9 @@
 function solution(s) {
 
     var alp = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
     for(var i=0; i<alp.length; i++) {
-       s=  s.replaceAll(alp[i], num[i]); 
+       s=  s.replaceAll(alp[i], i); 
     }
     
     
