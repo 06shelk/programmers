5f7b9a4b38a9124ef90f0b622aeb4946ca4b02a9
06shelk <88700824+06shelk@users.noreply.github.com>
2024-01-25 20:10:34 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 12.25 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -3,8 +3,7 @@ function solution(arr, queries) {
     
     for(var i=0; i< queries.length; i++){
         for(var j=0; j<arr.length; j++) {
-            const query = queries[i];
-            const [s, e, k] = query;
+            const [s, e, k] = queries[i];
             if(s <= j && j<=e && j%k ==0) arr[j]++;
         }
     }
