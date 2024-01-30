9b078cf95385890e1cfb99f5b81cba9ce592c6f3
06shelk <88700824+06shelk@users.noreply.github.com>
2024-01-30 13:06:25 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 13.04 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -3,24 +3,26 @@ function solution(arr, queries) {
     var array = [];
     
    for(var i=0; i< queries.length; i++){
-       var subArray = [];  
-       for(var j=0; j<arr.length; j++) {
-            const [s, e, k] = queries[i];
-            if(s <= j && j<=e && k < arr[j]) {
+       var subArray = [];
+       
+       const [s, e, k] = queries[i];
+       for(var j=s; j<=e; j++) {
+            
+            if(k < arr[j]) {
                 subArray.push(arr[j]);
             }
         }
 
        array.push(subArray.sort((a,b) => a-b));
-    }
-    
-    for(var i=0; i<array.length; i++) {
-        if(array[i][0] == null){
+       
+       
+       if(array[i][0] == null){
             answer.push(-1);
         }else {
             answer.push(array[i][0]);
         }
     }
+   
     
     
     return answer;
