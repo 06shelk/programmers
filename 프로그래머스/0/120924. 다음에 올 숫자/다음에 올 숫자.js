3ede32d0e47bf8009083c9feaec1bdf8b1677c91
06shelk <88700824+06shelk@users.noreply.github.com>
2024-01-31 17:01:54 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,11 @@
+function solution(common) {
+    var answer = 0;
+    
+    if( (common[0] + 2 * (common[1] - common[0])) == common[2]) { // 등차수열 식 a + (n-1)d
+        answer = common[0] + (common.length ) * (common[1] - common[0]) //등차
+    }else {
+        answer = common[0] * Math.pow((common[1] / common[0]), common.length) //등비
+    }
+    
+    return answer;
+} 
