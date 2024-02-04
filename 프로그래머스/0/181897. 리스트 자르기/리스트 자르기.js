536c2b4e5b2cb759cdf3b4f452601234952c1bcd
06shelk <88700824+06shelk@users.noreply.github.com>
2024-02-04 23:55:52 +0900

    [level 0] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -1,19 +1,14 @@
 function solution(n, slicer, num_list) {
-    var answer = [];
-    
-    if(n==1) {
-        answer = num_list.slice(0, slicer[1]+1);
-    }else if(n == 2){
-        answer = num_list.slice(slicer[0], num_list.length+1);
-    }else if(n == 3){
-        answer = num_list.slice(slicer[0], slicer[1]+1);
-    }else {
-        
-        num_list = num_list.slice(slicer[0], slicer[1]+1);
-        for(var i=0; i<num_list.length; i+= slicer[2]) {
-            answer.push(num_list[i]);
-        }
+    let [a, b, c] = [...slicer];
+
+    switch(n) {
+        case 1:
+            return num_list.slice(0, b + 1);
+        case 2:
+            return num_list.slice(a);
+        case 3:
+            return num_list.slice(a, b + 1);
+        case 4:
+            return num_list.slice(a, b + 1).filter((_,idx) => !(idx % c));
     }
-    
-    return answer;
 }
