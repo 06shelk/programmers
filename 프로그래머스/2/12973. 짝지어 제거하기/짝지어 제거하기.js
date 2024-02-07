b881cde60206f02cca88a60856051ec3391f616f
06shelk <88700824+06shelk@users.noreply.github.com>
2024-02-07 21:11:14 +0900

    [level 2] Title: 짝지어 제거하기, Time: 25.78 ms, Memory: 46.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    var answer = [];
+
+    for(var i=0; i< s.length; i++) {
+        if( answer.length > 0 && answer[answer.length - 1] == s[i]) {
+            answer.pop();
+        }else {
+            answer.push(s[i]);
+        }
+    }
+        
+    return answer == 0 ? 1 : 0;
+}
